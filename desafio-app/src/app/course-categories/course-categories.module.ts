0057e7653180c64d8e0290b046a2a3d0863f7ac6
Jonas Batista <jnsbatista2009@gmail.com>
2021-03-18 11:54:45 -0300

    modulos

---
+++ desafio-app/src/app/course-categories/course-categories.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseCategoriesComponent } from './course-categories.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CourseCategoriesComponent]
+})
+export class CourseCategoriesModule { }
---
+++ desafio-app/src/app/main-nav/main-nav.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainNavComponent } from './main-nav.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MainNavComponent]
+})
+export class MainNavModule { }
