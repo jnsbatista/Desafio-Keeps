8344e181ed859324f68f6ee7ba102b7afb59f238
Jonas Batista <jnsbatista2009@gmail.com>
2021-03-19 15:21:55 -0300

    cards

--- desafio-app/src/app/app.module.ts
+++ desafio-app/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { MainNavModule } from './main-nav/main-nav.module';
 import { AppComponent } from './app.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -18,6 +23,11 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     LayoutModule,
     MainNavModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
 
   ],
   providers: [],
--- desafio-app/src/app/courses/courses.module.ts
+++ desafio-app/src/app/courses/courses.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CoursesComponent } from './courses.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
   ],
-  declarations: [CoursesComponent]
+  declarations: [
+    CoursesComponent,
+  ]
 })
 export class CoursesModule { }
